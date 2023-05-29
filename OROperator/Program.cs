b2af2a7daaf576701d1e1aad23c30e139afcc77a
EnrikT <enrik.treier@tktk.ee>
2023-05-29 20:31:31 +0300

    add week 5 code

---
+++ OROperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90 OR Chemistry >= 90 OR Biology >= 90
+
+//Math && chemistry || Math && Biology || Chemistry && Biology
+
+int math, biology, chemistry;
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratz! You are in!");
+}
+else
+{
+    Console.WriteLine("You're not good enough!");
+}
